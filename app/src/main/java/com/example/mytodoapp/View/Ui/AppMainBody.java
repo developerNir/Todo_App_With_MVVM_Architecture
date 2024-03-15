1bb3fb99c0e4f35c47cc61817329b52e0d7bb0c6
nirapadak <naminopak@gmail.com>
2024-03-15 21:49:36 +0600

    drawer layout add and AllTodofragment add

--- app/src/main/java/com/example/mytodoapp/View/Ui/AppMainBody.java
+++ app/src/main/java/com/example/mytodoapp/View/Ui/AppMainBody.java
@@ -1,16 +1,28 @@
 package com.example.mytodoapp.View.Ui;
 
+import android.annotation.SuppressLint;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -18,36 +30,124 @@ import androidx.lifecycle.ViewModelProvider;
 import com.example.mytodoapp.R;
 import com.example.mytodoapp.Service.Model.LoginModel.LoginUser;
 import com.example.mytodoapp.Service.Model.RegisterModel;
+import com.example.mytodoapp.View.Ui.Fragment.AllTodoFragment;
 import com.example.mytodoapp.ViewModel.UserViewModel;
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.List;
 
 public class AppMainBody extends AppCompatActivity {
 
 
-    TextView textView;
-    UserViewModel userViewModel;
+
+    NavigationView navigationView;
+    FrameLayout frameLayout;
+    MaterialToolbar materialToolbar;
+    DrawerLayout drawerLayout;
     SharedPreferences sharedPreferences;
+    View headerView;
     SharedPreferences.Editor editor;
+    UserViewModel userViewModel;
 
 
 
+    TextView headText, headTextOtp;
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_app_main_body);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+
+
+
+
+        frameLayout = findViewById(R.id.frameLayout);
+        navigationView = findViewById(R.id.navigationView);
+        materialToolbar = findViewById(R.id.materialToolBar);
+        drawerLayout = findViewById(R.id.main);
+
+        // header View set ------------------------------
+        headerView = navigationView.getHeaderView(0);
+
+        // header Text View -------------------------------
+        headText = headerView.findViewById(R.id.textViewName);
+        headTextOtp = headerView.findViewById(R.id.textViewOtp);
+
+
+        // drawer Open and close --------------------------
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                AppMainBody.this, drawerLayout, materialToolbar, R.string.CloseDrawer, R.string.OpenDrawer
+        );
+
+        // Open drawer ------------------------
+        drawerLayout.addDrawerListener(toggle);
+
+
+
+        //sharePreference introduce ------------------------
+        sharedPreferences = getSharedPreferences(getString(R.string.app_name), MODE_PRIVATE);
+
+        // default Fragment --------------------------
+        replaceFragment(new AllTodoFragment());
+
+
+        // tool bar item select -----------------------------------
+        materialToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+
+                if (item.getItemId() == R.id.add_item){
+                    Toast.makeText(AppMainBody.this, "this to add item", Toast.LENGTH_SHORT).show();
+                }
+
+
+                return false;
+            }
         });
 
 
 
-        textView = findViewById(R.id.textView3);
+
+        // navigationView Item select ----------------------------------
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                if (menuItem.getItemId() ==R.id.myTodoItem){
+                    Toast.makeText(AppMainBody.this, "this is All todo item", Toast.LENGTH_SHORT).show();
+                    // close drawer --------------------------------------
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                } else if (menuItem.getItemId() == R.id.DeveloperItem) {
+                    Toast.makeText(AppMainBody.this, "Developer Item", Toast.LENGTH_SHORT).show();
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                } else if (menuItem.getItemId() == R.id.StartReteItem) {
+                    Toast.makeText(AppMainBody.this, "Star Rete Item", Toast.LENGTH_SHORT).show();
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                } else if (menuItem.getItemId() == R.id.logoutItem) {
+                    Toast.makeText(AppMainBody.this, "LogOut Item", Toast.LENGTH_SHORT).show();
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                }else {
+                    return false;
+                }
+
+                // return false =don't select item ----------and return true = selected item
+                return true;
+            }
+        });
+
+
+        // Live data Observer ---------------------------------
+
         // introduce SharePreferences ---------------------------
-        sharedPreferences = getSharedPreferences(getString(R.string.app_name), MODE_PRIVATE);
 
 
         // view Model Provider set -------------------------------------
@@ -61,7 +161,11 @@ public class AppMainBody extends AppCompatActivity {
                 @Override
                 public void onChanged(List<LoginUser> loginUser) {
                     Log.d("myLog", "Login Info == " + loginUser.get(0).getName());
-                    textView.setText(loginUser.get(0).getName());
+                    headText.setText(loginUser.get(0).getName());
+                    editor = sharedPreferences.edit();
+                    editor.putString("name", loginUser.get(0).getName());
+//                    editor.putString("token", registerModel.getToken());
+                    editor.apply();
                 }
             });
         }else {
@@ -77,8 +181,8 @@ public class AppMainBody extends AppCompatActivity {
                 public void onChanged(RegisterModel registerModel) {
 
                     if (registerModel.getSuccess()){
-                        textView.setText(registerModel.getToken());
                         editor = sharedPreferences.edit();
+                        editor.putString("name", registerModel.getUser().getName());
                         editor.putString("token", registerModel.getToken());
                         editor.apply();
                     }
@@ -89,14 +193,9 @@ public class AppMainBody extends AppCompatActivity {
             });
         }
 
+        String token = sharedPreferences.getString("token", null);
 
-
-        String token = sharedPreferences.getString("token",null);
-
-        textView.setText(token);
-
-
-
+        headTextOtp.setText(token);
 
 
 
@@ -108,4 +207,14 @@ public class AppMainBody extends AppCompatActivity {
 
 
     }
+
+    // fragment replace ------------------------------------
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+    }
+
+
 }
---
+++ app/src/main/java/com/example/mytodoapp/View/Ui/Fragment/AllTodoFragment.java
@@ -0,0 +1,112 @@
+package com.example.mytodoapp.View.Ui.Fragment;
+
+import static android.content.Context.MODE_PRIVATE;
+
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.mytodoapp.R;
+import com.example.mytodoapp.Service.Model.LoginModel.LoginUser;
+import com.example.mytodoapp.Service.Model.RegisterModel;
+import com.example.mytodoapp.View.Ui.AppMainBody;
+import com.example.mytodoapp.ViewModel.UserViewModel;
+
+import java.util.List;
+
+
+public class AllTodoFragment extends Fragment {
+
+    TextView textView;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    UserViewModel userViewModel;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_all_todo, container, false);
+
+
+        textView =view.findViewById(R.id.textView2);
+
+        // introduce SharePreferences ---------------------------
+        sharedPreferences = getContext().getSharedPreferences(getString(R.string.app_name), MODE_PRIVATE);
+
+
+        // view Model Provider set -------------------------------------
+        userViewModel = new ViewModelProvider(this).get(UserViewModel.class);
+
+
+        // Live data check and Null save ----------------------------
+        LiveData<List<LoginUser>> loginLiveData = userViewModel.getLoginLiveData();
+        if (loginLiveData != null) {
+            userViewModel.getLoginLiveData().observe((LifecycleOwner) getContext(), new Observer<List<LoginUser>>() {
+                @Override
+                public void onChanged(List<LoginUser> loginUser) {
+                    Log.d("myLog", "Login Info == " + loginUser.get(0).getName());
+                    textView.setText(loginUser.get(0).getName());
+                }
+            });
+        }else {
+            Toast.makeText(getContext(), "Live data is null", Toast.LENGTH_SHORT).show();
+        }
+
+
+        // get Token form View model --------------------------------
+        LiveData<RegisterModel> tokenAndRegisterData = userViewModel.getTokenWithRegister();
+        if (tokenAndRegisterData !=null){
+            userViewModel.getTokenWithRegister().observe(getViewLifecycleOwner(), new Observer<RegisterModel>() {
+                @Override
+                public void onChanged(RegisterModel registerModel) {
+
+                    if (registerModel.getSuccess()){
+                        textView.setText(registerModel.getToken());
+                        editor = sharedPreferences.edit();
+                        editor.putString("token", registerModel.getToken());
+                        editor.apply();
+                    }
+
+
+
+                }
+            });
+        }
+
+
+
+        String token = sharedPreferences.getString("token",null);
+
+        textView.setText(token);
+
+
+
+
+
+
+
+
+
+
+
+
+        return view;
+
+
+    }
+}
